2cdd594f66c0dc267099377d8f2b6d297d574d5b
sebaei <47517850+sebaei@users.noreply.github.com>
2023-09-29 18:53:06 +0300

    Add Cairo font to use as font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.scss";
+import "./fonts/Cairo-Regular.ttf";
+import "./fonts/Cairo-Bold.ttf";
 import Details from "./components/details.tsx";
 import Navbar from "./components/navbar.tsx";
 import Status from "./components/status.tsx";
